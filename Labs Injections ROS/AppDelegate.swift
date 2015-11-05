571a949377244fd45853588052063cb68bef47cb
FoolOfShadows <foolofshadows@gmail.com>
2015-11-05 12:35:48 -0600

    Initial Commit

---
+++ Labs Injections ROS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Labs Injections ROS
+//
+//  Created by Fool on 11/5/15.
+//  Copyright © 2015 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
